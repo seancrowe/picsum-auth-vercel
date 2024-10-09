4cfa6d83038a62f44b89d35ea33f2b71d48187c8
Sean Crowe <retrounlocked@gmail.com>
2024-10-09 16:47:35 -0500

    Logging headers

--- api/list.js
+++ api/list.js
@@ -1,5 +1,6 @@
 
 export default async function(req, res) {
+  console.log(req.headers)
   const { page = 1, limit = 30 } = req.query;
   const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`);
   const data = await response.json();
